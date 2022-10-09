214e6974287bb1c1591458ac209d302bb93ce013
Mohd Aman <mohdaman@Mohds-MacBook-Pro.local>
2022-10-09 21:35:15 +0530

    New Post

--- src/Headers.js
+++ src/Headers.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import "./Style.css";
-
 import "./Theam.css";
 import Navbar from "./Navbar";
-import ArrowDownwardSharpIcon from "@mui/icons-material/ArrowDownwardSharp";
+import Typewriter from "typewriter-effect";
 import MainPage from "./MainPage";
 export default function Headers() {
   return (
-    <>
+    <React.Fragment>
       <Navbar />
-      <Box className="main-color header-style">
-        <MainPage />
+      <Box className="header-style2 headerStyle">
+        <Box className=" container">
+          <Box className="Content">
+            <Typography variant="h2" component={"h2"} sx={{ fontWeight: 900 }}>
+              I'm UI/UX Designer
+            </Typography>
+            <Typography sx={{ marginTop: "10px" }}>
+              A tiny VS Code extension made up of a few commands that generate
+              and insert
+            </Typography>
+            {/* <Button sx={{ marginTop: "10px", backgroundColor: "black" }}>
+              Why Heire Me
+            </Button> */}
+          </Box>
+          <Box className="image"></Box>
+        </Box>
       </Box>
-    </>
+      <Typography>Hello</Typography>
+    </React.Fragment>
   );
 }
--- src/MainPage.js
+++ src/MainPage.js
@@ -6,17 +6,8 @@ import Grid from "@mui/material/Grid";
 import { Typography } from "@mui/material";
 export default function MainPage() {
   return (
-    <Box className="container" sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Typography variant="h3" component={"h2"} className="headerStyle">
-            Hello
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          <Typography>Hello</Typography>
-        </Grid>
-      </Grid>
-    </Box>
+    <React.Fragment>
+      <Typography className="headerStyle">Hello lorem</Typography>
+    </React.Fragment>
   );
 }
